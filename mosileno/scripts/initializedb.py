976ebf78f3f35e7bf2e2a250ea1250eb79b60ae6
Etienne Millon <me@emillon.org>
2012-11-08 22:28:47 +0100

    pep8'd initializedb.py

--- mosileno/scripts/initializedb.py
+++ mosileno/scripts/initializedb.py
@@ -7,18 +7,20 @@ from sqlalchemy import engine_from_config
 from pyramid.paster import (
     get_appsettings,
     setup_logging,
-    )
+)
 
 from ..models import (
     DBSession,
     Base,
-    )
+)
+
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
     print('usage: %s <config_uri>\n'
-          '(example: "%s development.ini")' % (cmd, cmd)) 
+          '(example: "%s development.ini")' % (cmd, cmd))
     sys.exit(1)
+
 
 def main(argv=sys.argv):
     if len(argv) != 2:
