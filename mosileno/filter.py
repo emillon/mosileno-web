d4b4e59798178e141e8aa9e16d207bd45a2d77ba
Etienne Millon <me@emillon.org>
2012-11-08 22:33:47 +0100

    pep8'd filter.py

--- mosileno/filter.py
+++ mosileno/filter.py
@@ -3,10 +3,10 @@ import markupsafe
 from lxml.html.clean import clean_html
 from lxml.etree import XMLSyntaxError
 
+
 def lx(s):
     try:
         r = clean_html(s)
     except XMLSyntaxError as e:
         r = markupsafe.escape(s)
     return r
-
