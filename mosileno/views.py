emillon/mosileno-web

commit af4d6fe6286e38b73de2ac7074bd8831d6842256

Make timeline more compact

mosileno/views.py
@@ -195,7 +195,8 @@ def _view_items(request, user, items, activelink=None):
     Arguments
         request : the current request
         user    : the user currently logged in
-        items   : the items to display
+        items   : the items to display, as a list of
+                  (item, feed title)
 
     Keyword arguments
         activelink : a function to set class=active
@@ -205,7 +206,7 @@ def _view_items(request, user, items, activelink=None):
     Returns
         a dictionary meant to be rendered by itemlist.mako.
     """
-    items = [(i, "collapse%d" % n) for (n, i) in enumerate(items)]
+    items = [(i, "collapse%d" % n, t) for (n, (i, t)) in enumerate(items)]
     feeds = DBSession.query(Feed)\
                      .join(Subscription)\
                      .filter(Subscription.user == user.id)
@@ -228,6 +229,7 @@ def view_myfeeds(request):
     me = authenticated_userid(request)
     user = DBSession.query(User).filter(User.name == me).one()
     items = DBSession.query(Item)\
+                     .add_columns(Feed.title)\
                      .join(Feed)\
                      .join(Subscription)\
                      .filter(Subscription.user == user.id)\

mosileno/static/css/mosileno.css
@@ -11,3 +11,17 @@ nav .nav-list {
     overflow: hidden;
     text-overflow:ellipsis;
 }
+
+.itemheader {
+    border-bottom: solid 1px #ddd;
+}
+
+.itemtitle {
+    display: inline;
+}
+
+.itemfeed {
+    display: inline;
+    color: #777;
+    padding-right: 10px;
+}

mosileno/templates/itemlist.mako
@@ -15,21 +15,25 @@ from mosileno.filter import lx
     </ul>
 </nav>
 
-<div class="accordion" id="accordion2">
-% for (item, genid) in items:
-    <div class="accordion-group">
-      <div class="accordion-heading">
-        <a class="accordion-toggle" data-toggle="collapse"
-           data-parent="#accordion2" href="#${genid}">
-          ${item.title}
-        </a>
-      </div>
-      <div id="${genid}" class="accordion-body collapse">
-        <div class="accordion-inner">
-          <a href="${item.link}">Go</a>
-          ${item.description | n, lx}
+<div>
+% for (item, genid, feedtitle) in items:
+    <div class="itemfull">
+        <div class="itemheader">
+            <a onclick="$('#${genid}').toggle()" href=#>
+                <div class="itemtitle">${item.title}</div>
+                <div class="pull-right">
+                    <div class="itemfeed">${feedtitle}</div>
+                    <i class="icon-thumbs-up"></i>
+                    <i class="icon-thumbs-down"></i>
+                </div>
+            </a>
         </div>
-      </div>
+        <div id="${genid}" style='display:none'>
+          <div>
+            <a href="${item.link}">Go</a>
+            ${item.description | n, lx}
+          </div>
+        </div>
     </div>
 % endfor
 </div>

commit 63fdb10732fb6262fb482e93629ef58b77ba5bf1

Fix regression in "view feed" page
(regression since af4d6fe6286e38b73de2ac7074bd8831d6842256)

mosileno/views.py
@@ -311,6 +311,7 @@ def view_myfeeds(request):
              )
 def view_feed(request):
     feedid = request.matchdict['feedid']
+    feedObj = DBSession.query(Feed).get(feedid)
     me = authenticated_userid(request)
     user = DBSession.query(User).filter(User.name == me).one()
 
@@ -324,6 +325,7 @@ def view_feed(request):
         return tpl(request)  # Better than nothing. TODO add an error message
 
     items = DBSession.query(Item).filter(Item.feed == subs[0].feed)
+    items = [(i, feedObj.title) for i in items]
 
     activeview = 'feed%s' % feedid
 