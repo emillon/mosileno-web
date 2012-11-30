77914e164827ba6676779c492b43cebf5079c1b7
syhw <gabriel.synnaeve@gmail.com>
2012-11-29 22:56:59 -0800

    new topics names + topic model hn40

--- brocoli/topics_tools.py
+++ brocoli/topics_tools.py
@@ -1,12 +1,12 @@
 import pickle
-import requests
+#import requests
 import numpy
 import sys
 from gensim import utils
 
-LEMMATIZE= False
+LEMMATIZE= True
 lda_model = None
-topic_model_filename = 'topic-model/hn%s.ldamodel'
+topic_model_filename = 'topic-model/hn40%s.ldamodel'
 
 def init_topic_model():
     " Necessary spaghetti code to fall back on models "
@@ -98,132 +98,178 @@ def topic_names(ldaobject):
     return zip(range(ldaobject.num_topics), topicnames)
 
 def manual_HN_topic_names(ldaobject):
-    topicnames = ['customer service',
-            'market finance',
-            'technological innovation',
-            'server performance',
-            'web frameworks',
-            'patents law',
-            'months',
-            'cloud computing',
-            'computer science',
-            'web apps',
-            'social web',
-            '(human) languages',
-            'free software',
-            '', # HN metadata
-            '', # user login junk
-            'unix/linux (OS)',
-            'tech news',
-            'public relations', # review/conf/PR
-            'space tech', # & light & energy
-            'news business',
-            'network security',
-            'social networks',
-            'video games',
-            'linux (kernel)',
-            'NYC',
-            '', # art & opinion & tech & news & business
-            'shoes',
-            'design (web)',
-            'academia',
-            'laptops & tablets',
-            'account/login', # login junk
-            '', # foursquare?
-            'social web',
-            'sexes differences',
-            'energy (science)',
-            'SF/Bay area',
-            'news (UK)',
-            'login/password', # login/pw junk
-            'spam?', # offensive / tumblr ?
-            'code',
-            'online shopping',
-            'names', # just names...
-            'globalization',
-            'time',
-            'months',
-            '(human) languages',
-            'publishing', # + privacy
-            'email',
-            'web sites',
-            '', # junk
-            'medecine',
-            'tech news (Cnet)',
-            'chips (hardware)',
-            'life',
-            'UK (Europe)',
-            'social news (Reddit)',
-            'Bloomberg',
-            'cars',
-            'startups',
-            'responses', # re re try wrtie
-            'music (clips)',
-            'SEO',
-            'government',
-            'argumentation',
-            '', # junk
-            '', # junk
-            'geek news',
-            'news recommendation', # + email
-            'developing countries',
-            '', # junk
-            'software development',
-            'search engines',
-            'social networks',
-            'food',
-            '', # junk
-            'funding',
-            '', # junk
-            'books',
-            'economy',
-            'education',
-            'sport news',
-            'mobile phones',
-            '', # junk
-            'movies', # ???
-            'questions',
-            'data structures',
-            'PG',
-            'wikipedia',
-            'html',
-            'artificial intelligence', # machine learning human intelligence
-            'news',
-            'software',
-            'thanks',
-            'photos',
-            'Apple',
-            'browsers',
-            'journals',
-            '', # junk
-            'programming languages',
-            'money/pricing']
-###    topn = ldaobject.num_topics
-###    bests = []
-###    topic_probas = []
-###    for topicid in range(ldaobject.num_topics):
-###        topic = ldaobject.state.get_lambda()[topicid]
-###        topic /= topic.sum()
-###        topic_probas.append(topic)
-###        bests.append(numpy.argsort(topic)[::-1][:topn])
-###    for topicid in range(ldaobject.num_topics):
-###        best10 = bests[topicid][:10]
-###        beststrl = []
-###        if LEMMATIZE:
-###            beststrl = [(topic_probas[topicid][i], ldaobject.id2word[i].split('/')[0])
-###                    for i in best10] # to remove POS-tag ("VB" in "be/VB")
-###        else:
-###            beststrl = [(topic_probas[topicid][i], ldaobject.id2word[i]) for i in best10]
-###        beststr = ' + '.join(['%.3f*%s' % v for v in beststrl])
-###        print "topic #", topicid, " described by:", topicnames[topicid]
-###        print beststr
+    topicnames = ['' for i in range(ldaobject.num_topics)]
+    if LEMMATIZE:
+        if ldaobject.num_topics == 40:
+            topicnames = ['facebook (social networks)',
+                    'months',
+                    'code',
+                    'password',
+                    'energy & transportation',
+                    'files', # github, shift...
+                    'economy', # money, market, banking
+                    'SF/Bay area',
+                    'domain names',
+                    'twitter (social networks)',
+                    'tech (& crunch)',
+                    'online marketplace',
+                    'microsoft',
+                    'login/account',
+                    'smartphones',
+                    'programming (languages)',
+                    'social news',
+                    '', # datum, page, search, link, result
+                    'design',
+                    'response?', # re, re, lot, problem, better
+                    'programming',
+                    'online/social news',
+                    'life',
+                    'NYC',
+                    'business',
+                    'tech news',
+                    'education',
+                    'amazon',
+                    'cloud',
+                    'web dev',
+                    '', # cs, image, thank, web, page
+                    'OS',
+                    'HN metadata', # points and votes
+                    'wikipedia & web',
+                    'months',
+                    'RoR',
+                    'patents law',
+                    'video games',
+                    '', # vote, zdnet, topic, search
+                    'startups']
+        else:
+            pass # TODO HAND SELECTED TOPICS FOR HN LEMMATIZED 100 TOPICS
+    else:
+        topicnames = ['customer service',
+                'market finance',
+                'technological innovation',
+                'server performance',
+                'web frameworks',
+                'patents law',
+                'months',
+                'cloud computing',
+                'computer science',
+                'web apps',
+                'social web',
+                '(human) languages',
+                'free software',
+                '', # HN metadata
+                '', # user login junk
+                'unix/linux (OS)',
+                'tech news',
+                'public relations', # review/conf/PR
+                'space tech', # & light & energy
+                'news business',
+                'network security',
+                'social networks',
+                'video games',
+                'linux (kernel)',
+                'NYC',
+                '', # art & opinion & tech & news & business
+                'shoes',
+                'design (web)',
+                'academia',
+                'laptops & tablets',
+                'account/login', # login junk
+                '', # foursquare?
+                'social web',
+                'sexes differences',
+                'energy (science)',
+                'SF/Bay area',
+                'news (UK)',
+                'login/password', # login/pw junk
+                'spam?', # offensive / tumblr ?
+                'code',
+                'online shopping',
+                'names', # just names...
+                'globalization',
+                'time',
+                'months',
+                '(human) languages',
+                'publishing', # + privacy
+                'email',
+                'web sites',
+                '', # junk
+                'medecine',
+                'tech news (Cnet)',
+                'chips (hardware)',
+                'life',
+                'UK (Europe)',
+                'social news (Reddit)',
+                'Bloomberg',
+                'cars',
+                'startups',
+                'responses', # re re try wrtie
+                'music (clips)',
+                'SEO',
+                'government',
+                'argumentation',
+                '', # junk
+                '', # junk
+                'geek news',
+                'news recommendation', # + email
+                'developing countries',
+                '', # junk
+                'software development',
+                'search engines',
+                'social networks',
+                'food',
+                '', # junk
+                'funding',
+                '', # junk
+                'books',
+                'economy',
+                'education',
+                'sport news',
+                'mobile phones',
+                '', # junk
+                'movies', # ???
+                'questions',
+                'data structures',
+                'PG',
+                'wikipedia',
+                'html',
+                'artificial intelligence', # machine learning human intelligence
+                'news',
+                'software',
+                'thanks',
+                'photos',
+                'Apple',
+                'browsers',
+                'journals',
+                '', # junk
+                'programming languages',
+                'money/pricing']
+
+    topn = ldaobject.num_topics
+    bests = []
+    topic_probas = []
+    for topicid in range(ldaobject.num_topics):
+        topic = ldaobject.state.get_lambda()[topicid]
+        topic /= topic.sum()
+        topic_probas.append(topic)
+        bests.append(numpy.argsort(topic)[::-1][:topn])
+    for topicid in range(ldaobject.num_topics):
+        best10 = bests[topicid][:10]
+        beststrl = []
+        if LEMMATIZE:
+            beststrl = [(topic_probas[topicid][i], ldaobject.id2word[i].split('/')[0])
+                    for i in best10] # to remove POS-tag ("VB" in "be/VB")
+        else:
+            beststrl = [(topic_probas[topicid][i], ldaobject.id2word[i]) for i in best10]
+        beststr = ' + '.join(['%.3f*%s' % v for v in beststrl])
+        print "topic #", topicid, " described by:", topicnames[topicid]
+        print beststr
     return zip(range(ldaobject.num_topics), topicnames)
 
 
-_lda_topic_names = None  # Lazily loaded
-
 init_topic_model()
 lda_topic_names = dict(manual_HN_topic_names(lda_model))
+
 
 def parse(text):
     def tokenize(text):
