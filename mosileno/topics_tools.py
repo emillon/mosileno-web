aed6963efeed0b4cce239a6f37fb66cdfca3de70
syhw <gabriel.synnaeve@gmail.com>
2012-11-29 18:25:59 -0800

    new topic names

--- mosileno/topics_tools.py
+++ mosileno/topics_tools.py
@@ -49,7 +49,7 @@ def topic_names(ldaobject):
         topic /= topic.sum()
         bests.append(numpy.argsort(topic)[::-1][:topn])
     for topicid in range(ldaobject.num_topics):
-        topicnames.append(ldaobject.id2word[bests[topicid][0]].split('/')[0] + " " 
+        topicnames.append(ldaobject.id2word[bests[topicid][0]].split('/')[0] + "/" 
                 + ldaobject.id2word[bests[topicid][1]].split('/')[0])
         ind = 0
         while ind < topn:
@@ -69,17 +69,158 @@ def topic_names(ldaobject):
                 continue
             # here we are not "contsearch"ing, so we have found a discriming
             # word as bests[topicid][ind]:
-            if ind == 0:
-                # if it's the first we keep the default
-                break
             topicnames[topicid] = ldaobject.id2word[bests[topicid][ind]].split('/')[0]
             break
-        best10 = bests[topicid][:10]
+        ind = 1
+        while ind < topn:
+            contsearch = False
+            # we seek the word bests[topicid][ind] in the bests words
+            # for others topic
+            for i,wl in enumerate(bests):
+                if i == topicid:
+                    continue
+                if bests[topicid][ind] in wl:
+                    # if we found bests[topicid][ind] in other topics' bests
+                    # words, we try and find a more discrimining word
+                    ind += 1
+                    contsearch = True
+                    break
+            if contsearch:
+                continue
+            # here we are not "contsearch"ing, so we have found a discriming
+            # word as bests[topicid][ind]:
+            if ind == 1:
+                # if it's the second we keep the default
+                break
+            topicnames[topicid] = topicnames[topicid] + '/' + ldaobject.id2word[bests[topicid][ind]].split('/')[0]
+            break
+    return zip(range(ldaobject.num_topics), topicnames)
+
+def manual_HN_topic_names(ldaobject):
+    topicnames = ['customer service',
+            'market finance',
+            'technological innovation',
+            'server performance',
+            'web frameworks',
+            'patents law',
+            'months',
+            'cloud computing',
+            'computer science',
+            'web apps',
+            'social web',
+            '(human) languages',
+            'free software',
+            '', # HN metadata
+            '', # user login junk
+            'unix/linux (OS)',
+            'tech news',
+            'public relations', # review/conf/PR
+            'space tech', # & light & energy
+            'news business',
+            'network security',
+            'social networks',
+            'video games',
+            'linux (kernel)',
+            'NYC',
+            '', # art & opinion & tech & news & business
+            'shoes',
+            'design (web)',
+            'academia',
+            'laptops & tablets',
+            'account/login', # login junk
+            '', # foursquare?
+            'social web',
+            'sexes differences',
+            'energy (science)',
+            'SF/Bay area',
+            'news (UK)',
+            'login/password', # login/pw junk
+            'spam?', # offensive / tumblr ?
+            'code',
+            'online shopping',
+            'names', # just names...
+            'globalization',
+            'time',
+            'months',
+            '(human) languages',
+            'publishing', # + privacy
+            'email',
+            'web sites',
+            '', # junk
+            'medecine',
+            'tech news (Cnet)',
+            'chips (hardware)',
+            'life',
+            'UK (Europe)',
+            'social news (Reddit)',
+            'Bloomberg',
+            'cars',
+            'startups',
+            'responses', # re re try wrtie
+            'music (clips)',
+            'SEO',
+            'government',
+            'argumentation',
+            '', # junk
+            '', # junk
+            'geek news',
+            'news recommendation', # + email
+            'developing countries',
+            '', # junk
+            'software development',
+            'search engines',
+            'social networks',
+            'food',
+            '', # junk
+            'funding',
+            '', # junk
+            'books',
+            'economy',
+            'education',
+            'sport news',
+            'mobile phones',
+            '', # junk
+            'movies', # ???
+            'questions',
+            'data structures',
+            'PG',
+            'wikipedia',
+            'html',
+            'artificial intelligence', # machine learning human intelligence
+            'news',
+            'software',
+            'thanks',
+            'photos',
+            'Apple',
+            'browsers',
+            'journals',
+            '', # junk
+            'programming languages',
+            'money/pricing']
+###    topn = ldaobject.num_topics
+###    bests = []
+###    topic_probas = []
+###    for topicid in range(ldaobject.num_topics):
+###        topic = ldaobject.state.get_lambda()[topicid]
+###        topic /= topic.sum()
+###        topic_probas.append(topic)
+###        bests.append(numpy.argsort(topic)[::-1][:topn])
+###    for topicid in range(ldaobject.num_topics):
+###        best10 = bests[topicid][:10]
+###        beststrl = []
+###        if LEMMATIZE:
+###            beststrl = [(topic_probas[topicid][i], ldaobject.id2word[i].split('/')[0])
+###                    for i in best10] # to remove POS-tag ("VB" in "be/VB")
+###        else:
+###            beststrl = [(topic_probas[topicid][i], ldaobject.id2word[i]) for i in best10]
+###        beststr = ' + '.join(['%.3f*%s' % v for v in beststrl])
+###        print "topic #", topicid, " described by:", topicnames[topicid]
+###        print beststr
     return zip(range(ldaobject.num_topics), topicnames)
 
 
 init_topic_model() # IMPORTANT !!!
-lda_topic_names = dict(topic_names(lda_model))
+lda_topic_names = dict(manual_HN_topic_names(lda_model))
 
 
 def parse(text):
