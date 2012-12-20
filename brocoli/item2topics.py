82cf20f124c67cb9513dab98205598a898c57476
syhw <gabriel.synnaeve@gmail.com>
2012-12-20 22:19:09 +0100

    Separating topics_tools and tika + new requests
     * topics_tools can now be imported in a lazy fashion
     * tika_tools contains a simple requests wrapper
     * requests new version (.json->.json())
     * cPickle instead of pickle (faster)

--- brocoli/item2topics.py
+++ brocoli/item2topics.py
@@ -11,13 +11,14 @@ from mosileno.models import (
 )
 from sqlalchemy import create_engine
 import transaction
-import topics_tools
+import tika_tools
 
 def get_topic_distrib(text):
     """ 
     gets the topics distribution and the extracted text (from Tika) 
     on the form [(topicid, probability)] for P(topic) > epsilon 
     """
+    import topics_tools
     lda = topics_tools.lda_model
     return lda[lda.id2word.doc2bow(topics_tools.parse(text))]
 
@@ -30,7 +31,7 @@ for item in items:
     print item.id
     with transaction.manager:
         item = DBSession.query(Item).get(item.id)
-        data = topics_tools.tika(item.link)
+        data = tika_tools.tika(item.link)
         if data is None:
             continue
         tops = get_topic_distrib(data)
